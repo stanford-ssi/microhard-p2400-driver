e89e0836dacad73ceef47204990dd09c46dc1dc1
Elijah Kim <reachelijah@gmail.com>
2021-04-22 00:17:08 -0700

    Correct arduino sketch

--- arduino_serial.ino
+++ arduino_serial.ino
@@ -6,24 +6,22 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-
   establishContact();  // send a byte to establish contact until receiver responds
 }
 
 void loop() {
   // if we get a valid byte, read analog ins:
   if (Serial.available() > 0) {
-//    // get incoming byte:
-//    inByte = Serial.read();
-//    Serial.write(inByte);
-    Serial.println("made contact. ready");
-    delay(1000);
+    // get incoming byte:
+    inByte = Serial.read();
+    Serial.write(inByte);
+    delay(200);
   }
 }
 
 void establishContact() {
   while (Serial.available() <= 0) {
-    Serial.println("available");
-    delay(1000);
+    Serial.write(5);
+    delay(200);
   }
 }
